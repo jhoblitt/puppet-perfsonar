07a5ff82578fad9cc44a4a0515b561ccf8efa33d
Joshua Hoblitt <jhoblitt@cpan.org>
2014-01-26 15:43:58 -0700

    implement anchor pattern in perfsonar class

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,12 +15,22 @@ class perfsonar (
   validate_bool($enable_bwctl)
   validate_bool($enable_owamp)
 
+  anchor { "${name}::begin": }
+
   if $manage_repo {
     include perfsonar::repo
+
+    Anchor["${name}::begin"] ->
+    Class['perfsonar::repo'] ->
+    Anchor["${name}::end"]
   }
 
   if $enable_bwctl {
     include perfsonar::bwctl
+
+    Anchor["${name}::begin"] ->
+    Class['perfsonar::bwctl'] ->
+    Anchor["${name}::end"]
 
     if $manage_repo {
       Class['perfsonar::repo'] -> Class['perfsonar::bwctl']
@@ -30,8 +40,14 @@ class perfsonar (
   if $enable_owamp {
     include perfsonar::owamp
 
+    Anchor["${name}::begin"] ->
+    Class['perfsonar::owamp'] ->
+    Anchor["${name}::end"]
+
     if $manage_repo {
       Class['perfsonar::repo'] -> Class['perfsonar::owamp']
     }
   }
+
+  anchor { "${name}::end": }
 }
