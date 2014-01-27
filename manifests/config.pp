9e5df8a1ecdb12d5459fdee3a393b591c8a4f4e5
Joshua Hoblitt <jhoblitt@cpan.org>
2014-01-27 12:24:59 -0700

    remove accidental commit of perfsonar::config define

--- manifests/config.pp
+++
@@ -1,9 +0,0 @@
-# private type
-define perfsonar::config (
-  $template =  'foo',
-) {
-  if $caller_module_name != $module_name {
-    fail("Use of private class ${name} by ${caller_module_name}")
-  }
-
-}
